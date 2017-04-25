a84539f3403bf3f8993c74abcbe466d979c7664e
Matt <mjschau2@illinois.edu>
2017-04-25 16:24:51 -0500

    began k-means setup

--- k-means.py
+++ k-means.py
@@ -1,6 +1,24 @@
 
+import numpy as np
 
 
+if __name__ =='__main__':
+	
+
+
+	k = 3
+	clusters = []
+	for i in range(0,k):
+		clusters.append(np.random.uniform(low=0.0, high=10.0, size=4)))
+	k = 4
+	clusters_two = []
+	for i in range(0,k):
+		clusters_two.append(np.random.uniform(low=0.0, high=10.0, size=4)))
+	k = 5
+	clusters_three = []
+	for i in range(0,k):
+		clusters_three.append(np.random.uniform(low=0.0, high=10.0, size=4)))
+	
 
 
 
