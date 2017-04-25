590165e55546c7d9f642714456cf0950dba65667
Matt <mjschau2@illinois.edu>
2017-04-25 16:27:59 -0500

    directions

--- k-means.py
+++ k-means.py
@@ -19,6 +19,11 @@ if __name__ =='__main__':
 	for i in range(0,k):
 		clusters_three.append(np.random.uniform(low=0.0, high=10.0, size=4)))
 	
+	#loop through each line of data point for 5,10,20 set times
+	#record accuracy for each cluster and each number of times
+	#graph that stuff
+	#find the most effective clustering
+	#use sklearn to plot the 3d cluster in 4 different ways 
 
 
 
